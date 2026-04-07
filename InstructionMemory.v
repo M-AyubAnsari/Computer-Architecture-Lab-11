dfb43f8a0add87a8d02ac9ef117984f2cd060a92
M-AyubAnsari <ma10034@st.habib.edu.pk>
2026-04-07 23:45:17 +0500

    Updated InstructionMemory.v Uploaded

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -9,8 +9,7 @@ integer i;
 initial begin
     for (i = 0; i < 256; i = i + 1)
         memory[i] = 32'b0;
-    // Load your program here
-    memory[0] = 32'h00000013; // NOP (addi x0,x0,0)
+    $readmemh("Program.mem", memory);
 end
 
 assign Instruction = memory[addr[9:2]];
